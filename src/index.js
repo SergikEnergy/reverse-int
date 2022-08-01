b5d1eeef3db9b1414e576684a862e0d825d33e10
SergikEnergy <kis8883244@gmail.com>
2022-08-01 23:21:19 +0300

    feat: add the solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let n2 = Math.abs(n);
+    let StringArray = String(n2).split("");
+    let reverseString = StringArray.reverse();
+    let TempReverse = reverseString.join("");
+    return Number(TempReverse);
+};
